6396d046f3314b3acfe80e8e7b741115a83993ba
filip <filipcichyf14@gmail.com>
2021-10-05 21:54:34 +0200

    added simple authorization

---
+++ src/main/java/pl/auction_service/user/User.java
@@ -0,0 +1,57 @@
+package pl.auction_service.user;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Collection;
+
+@Entity(name = "users")
+public class User implements UserDetails {
+
+    @Id()
+    private Long id;
+    private String username;
+    private String password;
+    private int roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
